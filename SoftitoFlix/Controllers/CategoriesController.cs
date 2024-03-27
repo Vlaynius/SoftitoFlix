077b7fb73b59579b77ca5cea6ad49cf7656ef63f
Vlaynius <onurgsaydin@gmail.com>
2024-03-27 15:43:06 +0300

    Controllers
    Category, User, Director

--- SoftitoFlix/Controllers/CategoriesController.cs
+++ SoftitoFlix/Controllers/CategoriesController.cs
@@ -1,5 +1,5 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SoftitoFlix.Data;
 using SoftitoFlix.Models;
 
@@ -18,102 +18,49 @@ namespace SoftitoFlix.Controllers
 
         // GET: api/Categories
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        public ActionResult<List<Category>> GetCategories()
         {
-          if (_context.Categories == null)
-          {
-              return NotFound();
-          }
-            return await _context.Categories.ToListAsync();
+            return  _context.Categories.ToList();
         }
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Category>> GetCategory(int id)
+        [Authorize]
+        public  ActionResult<Category> GetCategory(int id)
         {
-          if (_context.Categories == null)
-          {
-              return NotFound();
-          }
-            var category = await _context.Categories.FindAsync(id);
-
+            Category? category =  _context.Categories.Find(id);
             if (category == null)
             {
                 return NotFound();
             }
-
             return category;
         }
 
         // PUT: api/Categories/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCategory(int id, Category category)
+        [Authorize(Roles = "ContentAdmin")]
+        public void PutCategory( Category category)
         {
-            if (id != category.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(category).State = EntityState.Modified;
-
+            _context.Categories.Update(category);
             try
             {
-                await _context.SaveChangesAsync();
+                 _context.SaveChanges();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CategoryExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            catch (Exception)
+            { }
         }
 
         // POST: api/Categories
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Category>> PostCategory(Category category)
+        [Authorize(Roles = "ContentAdmin")]
+        public int PostCategory(Category category)
         {
-          if (_context.Categories == null)
-          {
-              return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
-          }
             _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetCategory", new { id = category.Id }, category);
+            _context.SaveChanges();
+            return category.Id;
         }
 
-        // DELETE: api/Categories/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCategory(int id)
-        {
-            if (_context.Categories == null)
-            {
-                return NotFound();
-            }
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool CategoryExists(int id)
-        {
-            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
 }
---
+++ SoftitoFlix/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoftitoFlix.Data;
+using SoftitoFlix.Models;
+
+namespace SoftitoFlix.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public UserController(/*ApplicationDbContext context,*/ SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
+        {
+            //_context = context;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public  ActionResult<List<ApplicationUser>> GetUsers()
+        {
+          if (_signInManager.UserManager.Users == null)
+          {
+              return NotFound();
+          }
+            return  _signInManager.UserManager.Users.ToList();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public  ActionResult<ApplicationUser> GetApplicationUser(long id)
+        {
+          if (_signInManager.UserManager.Users == null)
+          {
+              return NotFound();
+          }
+          ApplicationUser? applicationUser =  _signInManager.UserManager.Users.Where(r=>r.Id == id).FirstOrDefault();
+          if (applicationUser == null)
+          {
+              return NotFound();
+          }
+          return applicationUser;
+        }
+
+        // PUT: api/User/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public ActionResult PutApplicationUser( ApplicationUser applicationUser)
+        {
+            ApplicationUser? existingUser = _signInManager.UserManager.FindByIdAsync(applicationUser.Id.ToString()).Result;
+            if(existingUser == null)
+            {
+                return NotFound();
+            }
+            existingUser.Name = applicationUser.Name;
+            existingUser.BirthDate = applicationUser.BirthDate;
+            existingUser.Email = applicationUser.Email;
+            existingUser.Passive = applicationUser.Passive;
+            existingUser.PhoneNumber = applicationUser.PhoneNumber;
+            existingUser.UserName = applicationUser.UserName;
+            _signInManager.UserManager.UpdateAsync(existingUser);
+            return NoContent();
+        }
+
+        // POST: api/User
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public long PostApplicationUser(ApplicationUser applicationUser, string Password)
+        {
+            _signInManager.UserManager.CreateAsync(applicationUser, Password).Wait();
+            return applicationUser.Id;
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteApplicationUser(long id)
+        {
+            ApplicationUser? applicationUser = _signInManager.UserManager.FindByIdAsync(id.ToString()).Result;
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+            applicationUser.Passive = true;
+            _signInManager.UserManager.UpdateAsync(applicationUser).Wait();
+            return NoContent();
+        }
+
+    }
+}
